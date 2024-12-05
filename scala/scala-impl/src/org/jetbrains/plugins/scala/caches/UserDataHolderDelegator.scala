d51eac1b72af14748e27cb27e58aec908e92e32c
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-12-05 16:15:26 +0100

    [cache] return temporary UserDataHolder when holder is already disposed #SCL-23288 fixed

--- scala/scala-impl/src/org/jetbrains/plugins/scala/caches/UserDataHolderDelegator.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/caches/UserDataHolderDelegator.scala
@@ -1,7 +1,7 @@
 package org.jetbrains.plugins.scala.caches
 
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.util.{Disposer, UserDataHolderBase, UserDataHolderEx}
+import com.intellij.openapi.util.{CheckedDisposable, Disposer, UserDataHolderBase, UserDataHolderEx}
 
 import java.util.concurrent.ConcurrentHashMap
 import scala.annotation.nowarn
@@ -10,16 +10,23 @@ object UserDataHolderDelegator {
   private val delegates = new ConcurrentHashMap[Disposable, UserDataHolderBase]
 
   @nowarn("cat=deprecation")
-  def userDataHolderFor(holder: Disposable): UserDataHolderEx =
-    delegates.computeIfAbsent(holder, holder => {
-      // Ideally we wouldn't need holder to be a disposable to support arbitrary objects.
-      // delegates could then be a weak hashmap and the userdata gets removed out when holder is collected.
-      // Unfortunately this can lead to memory leaks when values in the userdata reference the holder.
-      // In that case the holder wouldn't get collected and subsequently not removed from delegates.
-      // Instead we force holder to be a Disposable and relay on the deposing mechanism to remove it from the map
-      assert(!Disposer.isDisposed(holder))
-      Disposer.register(holder, () => delegates.remove(holder))
-
+  def userDataHolderFor(holder: Disposable): UserDataHolderEx = {
+    if (Disposer.isDisposed(holder)) {
+      // Return a temporary UserDataHolder
+      // This is just to alleviate problems when a Disposable is being disposed
       new UserDataHolderBase
-    })
+    } else {
+      delegates.computeIfAbsent(holder, holder => {
+        // Ideally we wouldn't need holder to be a disposable to support arbitrary objects.
+        // delegates could then be a weak hashmap and the userdata gets removed out when holder is collected.
+        // Unfortunately this can lead to memory leaks when values in the userdata reference the holder.
+        // In that case the holder wouldn't get collected and subsequently not removed from delegates.
+        // Instead we force holder to be a Disposable and relay on the deposing mechanism to remove it from the map
+        //assert(!Disposer.isDisposed(holder))
+        Disposer.register(holder, () => delegates.remove(holder))
+
+        new UserDataHolderBase
+      })
+    }
+  }
 }
