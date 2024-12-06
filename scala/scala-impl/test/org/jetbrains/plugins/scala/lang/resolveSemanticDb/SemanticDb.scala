804fd823d102bde43288479ffb96e0adcbb75e09
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2024-12-06 17:21:14 +0100

    extracted scala.meta Locator into scala-meta-impl #SCL-23181 fixed

--- scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/SemanticDb.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/lang/resolveSemanticDb/SemanticDb.scala
@@ -2,7 +2,6 @@ package org.jetbrains.plugins.scala.lang.resolveSemanticDb
 
 import java.nio.file.{Files, Path}
 import scala.collection.mutable
-import scala.meta.internal.semanticdb.Locator
 
 
 case class SDbRef(symbol: String, position: TextPos, endPosition: TextPos, targetPosition: Option[TextPos]) {
@@ -55,37 +54,11 @@ case class SemanticDbStore(files: Seq[SDbFile]) {
 }
 
 object SemanticDbStore {
-  def fromSemanticDbPath(path: Path): SemanticDbStore = {
-    type UnfinishedRef = (String, TextPos, TextPos)
-    val positionOfSymbols = mutable.Map.empty[String, TextPos]
-    val unfinishedFiles = mutable.Map.empty[String, Seq[UnfinishedRef]]
-    Locator(path) { (_, payload) =>
-      for (doc <- payload.documents) {
-        val refs = Seq.newBuilder[UnfinishedRef]
-        for (occurrence <- doc.occurrences) {
-          def start = TextPos.ofStart(occurrence.range.get)
-          def end = TextPos.ofEnd(occurrence.range.get)
-          if (occurrence.role.isDefinition) positionOfSymbols += occurrence.symbol -> start
-          else if (occurrence.role.isReference) refs += ((occurrence.symbol, start, end))
-        }
-        unfinishedFiles += doc.uri -> refs.result()
-      }
-    }
-
-    val files = unfinishedFiles.iterator
-      .map {
-        case (path, unfinishedRefs) =>
-          val refs =
-            for ((symbol, start, end) <- unfinishedRefs)
-              yield SDbRef(symbol, start, end, targetPosition = positionOfSymbols.get(symbol))
-          SDbFile(path, refs.sorted)
-      }
-      .toSeq
-      .sortBy(_.path)
-
-    SemanticDbStore(files)
-  }
-
+  /**
+   * Reads a SemanticDbStore from a text file.
+   * For the creation of these text files see [[SemanticDbFromScalaMeta.fromSemanticDbPath]].
+   * It is created in [[AfterUpdateDottyVersionScript]].
+   */
   def fromTextFile(path: Path): SemanticDbStore =
     fromText(Files.readString(path))
 
