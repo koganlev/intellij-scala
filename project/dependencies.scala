58bd3a803e6b9160f12dad84aa6ca6bb2a54694f
builduser <builduser@localhost>
2025-01-08 10:15:11 +0000

    Update intellijVersion to 251.14649.2

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.10.7"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "251.14649"
+  val intellijVersion = "251.14649.2"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
