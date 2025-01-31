aef5e34f7b5e56b2b6088fe06056d49c1d479acf
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-01-31 21:41:05 +0100

    Incremental highlighting: refactoring, #SCL-23216

--- scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/Updater.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/Updater.scala
@@ -2,7 +2,7 @@ package org.jetbrains.plugins.scala.incremental
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.ex.{EditorEx, MarkupModelEx}
+import com.intellij.openapi.editor.ex.EditorEx
 import com.intellij.openapi.util.{Key, TextRange}
 import com.intellij.psi.PsiManager
 import org.jetbrains.plugins.scala.incremental.Updater._
@@ -32,43 +32,46 @@ private class Updater(editor: Editor) {
   private def doUpdate(): Unit = {
     val visibleRange = editor.getUserData(EditorArea.VISIBLE_RANGE_KEY)
 
-    val markupModel = editor.asInstanceOf[EditorEx].getFilteredDocumentMarkupModel
-    concealMarksOutside(visibleRange, markupModel)
-    revealMarksInside(visibleRange, markupModel)
+    concealErrorStripeMarksOutside(visibleRange, editor)
+    revealErrorStripeMarksInside(visibleRange, editor)
 
     val newlyVisibleRange = if (previousVisibleRange == null) visibleRange else visibleRange.diff(previousVisibleRange)
 
     val document = PsiManager.getInstance(editor.getProject).findFile(editor.getVirtualFile).getViewProvider.getDocument
     val daemon = DaemonCodeAnalyzer.getInstance(editor.getProject)
     daemon.combineDirtyScopes(document, newlyVisibleRange)
-    daemon.stopProcess(true)
+    daemon.stopProcess(/* restart = */ true)
 
     previousVisibleRange = visibleRange
   }
+}
 
-  private def revealMarksInside(visibleRange: TextRange, markupModel: MarkupModelEx) = {
-    markupModel.processRangeHighlightersOverlappingWith(visibleRange.getStartOffset, visibleRange.getEndOffset, highlighter => {
-      val savedColor = highlighter.getUserData(ErrorStripeMarkColorKey)
-      if (savedColor != null) {
-        highlighter.setErrorStripeMarkColor(savedColor)
-        highlighter.putUserData(ErrorStripeMarkColorKey, null)
-      }
-      true
-    })
-  }
+private object Updater {
+  private val ERROR_STRIPE_MARK_COLOR_KEY = Key.create[Color]("error_stripe_mark_color")
 
-  private def concealMarksOutside(visibleRange: TextRange, markupModel: MarkupModelEx) = {
+  private def concealErrorStripeMarksOutside(visibleRange: TextRange, editor: Editor) = {
+    val markupModel = editor.asInstanceOf[EditorEx].getFilteredDocumentMarkupModel
+
     markupModel.processRangeHighlightersOutside(visibleRange.getStartOffset, visibleRange.getEndOffset, highlighter => {
       val actualColor = highlighter.getErrorStripeMarkColor(editor.getColorsScheme)
       if (!highlighter.isThinErrorStripeMark && actualColor != null) {
-        highlighter.putUserData(ErrorStripeMarkColorKey, actualColor)
+        highlighter.putUserData(ERROR_STRIPE_MARK_COLOR_KEY, actualColor)
         highlighter.setErrorStripeMarkColor(null)
       }
       true
     })
   }
-}
 
-private object Updater {
-  private val ErrorStripeMarkColorKey = Key.create[Color]("error_stripe_mark_color")
+  private def revealErrorStripeMarksInside(visibleRange: TextRange, editor: Editor) = {
+    val markupModel = editor.asInstanceOf[EditorEx].getFilteredDocumentMarkupModel
+
+    markupModel.processRangeHighlightersOverlappingWith(visibleRange.getStartOffset, visibleRange.getEndOffset, highlighter => {
+      val savedColor = highlighter.getUserData(ERROR_STRIPE_MARK_COLOR_KEY)
+      if (savedColor != null) {
+        highlighter.setErrorStripeMarkColor(savedColor)
+        highlighter.putUserData(ERROR_STRIPE_MARK_COLOR_KEY, null)
+      }
+      true
+    })
+  }
 }
--- scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/package.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/package.scala
@@ -6,7 +6,7 @@ import com.intellij.openapi.editor.Document
 import com.intellij.openapi.util.TextRange
 
 package object incremental {
-  private final val Reason = "Incremental highlighting"
+  private final val REASON = "Incremental highlighting"
 
   private val combineDirtyScopesMethod = {
     val method = classOf[FileStatusMap].getDeclaredMethod("combineDirtyScopes", classOf[Document], classOf[TextRange], classOf[Object])
@@ -23,12 +23,12 @@ package object incremental {
   private[incremental] implicit class DaemonCodeAnalyzerExt(private val daemon: DaemonCodeAnalyzer) extends AnyVal {
     // com.intellij.codeInsight.daemon.impl.FileStatusMap.combineDirtyScopes
     def combineDirtyScopes(document: Document, scope: TextRange): Unit = {
-      combineDirtyScopesMethod.invoke(daemon.asInstanceOf[DaemonCodeAnalyzerEx].getFileStatusMap, document, scope, Reason)
+      combineDirtyScopesMethod.invoke(daemon.asInstanceOf[DaemonCodeAnalyzerEx].getFileStatusMap, document, scope, REASON)
     }
 
     // com.intellij.codeInsight.daemon.impl.DaemonCodeAnalyzerImpl.stopProcess
     def stopProcess(toRestartAlarm: Boolean): Unit = {
-      stopProcessMethod.invoke(daemon, toRestartAlarm, Reason)
+      stopProcessMethod.invoke(daemon, toRestartAlarm, REASON)
     }
   }
 
