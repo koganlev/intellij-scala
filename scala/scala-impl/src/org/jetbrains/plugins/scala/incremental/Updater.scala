b2833b2d3c22d7d830406c56d4359c6926f7b073
Pavel Fatin <pavel.fatin@jetbrains.com>
2025-02-03 20:10:29 +0100

    Incremental highlighting: adjust regular MarkupModel too, #SCL-23216

--- scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/Updater.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/incremental/Updater.scala
@@ -5,7 +5,8 @@ import incremental.Updater._
 
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.editor.Editor
-import com.intellij.openapi.editor.ex.EditorEx
+import com.intellij.openapi.editor.colors.EditorColorsScheme
+import com.intellij.openapi.editor.ex.{EditorEx, MarkupModelEx}
 import com.intellij.openapi.util.{Key, TextRange}
 import com.intellij.psi.PsiManager
 
@@ -32,8 +33,11 @@ private class Updater(editor: Editor) {
   private def update(): Unit = {
     val visibleRange = VisibleRange.in(editor)
 
-    concealErrorStripeMarksOutside(visibleRange, editor)
-    revealErrorStripeMarksInside(visibleRange, editor)
+    val editorEx = editor.asInstanceOf[EditorEx]
+    Seq(editorEx.getMarkupModel, editorEx.getFilteredDocumentMarkupModel).foreach { model =>
+      concealErrorStripeMarksOutside(visibleRange, model, editor.getColorsScheme)
+      revealErrorStripeMarksInside(visibleRange, model)
+    }
 
     val newlyVisibleRange = if (previousVisibleRange == null) visibleRange else visibleRange.diff(previousVisibleRange)
 
@@ -51,11 +55,9 @@ private object Updater {
 
   private val ERROR_STRIPE_MARK_COLOR_KEY = Key.create[Color]("error_stripe_mark_color")
 
-  private def concealErrorStripeMarksOutside(visibleRange: TextRange, editor: Editor) = {
-    val markupModel = editor.asInstanceOf[EditorEx].getFilteredDocumentMarkupModel
-
+  private def concealErrorStripeMarksOutside(visibleRange: TextRange, markupModel: MarkupModelEx, colorScheme: EditorColorsScheme): Unit = {
     markupModel.processRangeHighlightersOutside(visibleRange.getStartOffset, visibleRange.getEndOffset, highlighter => {
-      val actualColor = highlighter.getErrorStripeMarkColor(editor.getColorsScheme)
+      val actualColor = highlighter.getErrorStripeMarkColor(colorScheme)
       if (!highlighter.isThinErrorStripeMark && actualColor != null) {
         highlighter.putUserData(ERROR_STRIPE_MARK_COLOR_KEY, actualColor)
         highlighter.setErrorStripeMarkColor(null)
@@ -64,9 +66,7 @@ private object Updater {
     })
   }
 
-  private def revealErrorStripeMarksInside(visibleRange: TextRange, editor: Editor) = {
-    val markupModel = editor.asInstanceOf[EditorEx].getFilteredDocumentMarkupModel
-
+  private def revealErrorStripeMarksInside(visibleRange: TextRange, markupModel: MarkupModelEx): Unit = {
     markupModel.processRangeHighlightersOverlappingWith(visibleRange.getStartOffset, visibleRange.getEndOffset, highlighter => {
       val savedColor = highlighter.getUserData(ERROR_STRIPE_MARK_COLOR_KEY)
       if (savedColor != null) {
