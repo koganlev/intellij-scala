35da697793d9a1656b38f8890bcfa52d08462495
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-02-05 11:20:09 +0100

    [debugger] Rewrite ScalaSuperDelegate according to the latest platform recommendations to avoid deprecation warnings #IDEA-366620

--- scala/debugger/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaSuperEvaluator.scala
+++ scala/debugger/src/org/jetbrains/plugins/scala/debugger/evaluation/evaluator/ScalaSuperEvaluator.scala
@@ -1,16 +1,12 @@
 package org.jetbrains.plugins.scala.debugger.evaluation.evaluator
 
 import com.intellij.debugger.engine.evaluation.EvaluationContextImpl
-import com.intellij.debugger.engine.evaluation.expression.{Evaluator, Modifier}
-
-import scala.annotation.nowarn
+import com.intellij.debugger.engine.evaluation.expression.{Evaluator, ModifiableEvaluator, ModifiableValue}
 
 trait ScSuperEvaluator
 
 class ScalaSuperEvaluator(iterations: Int = 0) extends ScalaThisEvaluator(iterations) with ScSuperEvaluator
 
-class ScalaSuperDelegate(delegate: Evaluator) extends Evaluator with ScSuperEvaluator {
-  override def evaluate(context: EvaluationContextImpl): AnyRef = delegate.evaluate(context)
-
-  override def getModifier: Modifier = delegate.getModifier: @nowarn("cat=deprecation") // IDEA-366620
+class ScalaSuperDelegate(delegate: Evaluator) extends ModifiableEvaluator with ScSuperEvaluator {
+  override def evaluateModifiable(context: EvaluationContextImpl): ModifiableValue = delegate.evaluateModifiable(context)
 }
