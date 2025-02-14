007c1fd2cd2f6f5dd394361dd9b8e98037b16533
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-02-14 11:40:14 +0100

    [compiler] Remove the Zinc UI Designer plugin form compiler integration workaround #SCL-23611 fixed
    - I've looked into the details of the UI Designer plugin form compiler.
    - I believe the plugin has been rewritten quite a long time ago to be a self-contained compiler and instrumenter of UI forms.
    - The plugin code is not referenced from any other IDEA platform sources, and especially not from the JavaBuilder JPS Java compiler and KotlinBuilder JPS Kotlin compiler.
    - I also manually tested the differences between the Scala Zinc and Scala IDEA incremental compilers and their integration with the forms compiler. I didn't find any differences. Both full and incremental builds containing changes to forms (and manual changes to form classes) work as expected.
    - Furthermore, I've been using the Zinc incremental compiler for developing our plugin in the last couple of months, with the Zinc UI Designer plugin form compiler in its current non-functioning state and didn't experience any problems.
    - I've decided to remove this code instead of continuing to maintain it.
    - If we do discover that this workaround is still needed going forward, we can bring it back and update it.

--- scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/local/IdeClientSbt.scala
+++ scala/compiler-jps/src/org/jetbrains/jps/incremental/scala/local/IdeClientSbt.scala
@@ -1,8 +1,6 @@
 package org.jetbrains.jps.incremental.scala
 package local
 
-import com.intellij.openapi.util.Key
-import com.intellij.openapi.util.io.FileUtil
 import org.jetbrains.jps.ModuleChunk
 import org.jetbrains.jps.builders.{BuildRootDescriptor, BuildTarget}
 import org.jetbrains.jps.incremental.ModuleLevelBuilder.OutputConsumer
@@ -10,9 +8,6 @@ import org.jetbrains.jps.incremental.fs.CompilationRound
 import org.jetbrains.jps.incremental.{CompileContext, FSOperations}
 
 import java.nio.file.{Path, Paths}
-import java.util
-import scala.jdk.CollectionConverters._
-import scala.util.control.Exception._
 
 class IdeClientSbt(compilerName: String,
                    context: CompileContext,
@@ -22,7 +17,6 @@ class IdeClientSbt(compilerName: String,
         extends IdeClient(compilerName, context, chunk) {
 
   override def generated(source: java.io.File, outputFile: java.io.File, name: String): Unit = {
-    invalidateBoundForms(source)
     val target = sourceToTarget(source.toPath).getOrElse {
       throw new RuntimeException("Unknown source file: " + source)
     }
@@ -32,33 +26,5 @@ class IdeClientSbt(compilerName: String,
 
   override def sourceStarted(source: String): Unit = {
     FSOperations.markDirty(context, CompilationRound.NEXT, Paths.get(source).toFile)
-  }
-
-  // TODO needs to be rewritten according to the latest changes in the platform. This code is currently not executed.
-  import java.io.File
-
-  // TODO Expect JPS compiler in UI-designer to take generated class events into account
-  private val FormsToCompileKey = catching(classOf[ClassNotFoundException], classOf[NoSuchFieldException]).opt {
-    val field = Class.forName("org.jetbrains.jps.uiDesigner.compiler.FormsBuilder").getDeclaredField("FORMS_TO_COMPILE")
-    field.setAccessible(true)
-    field.get(null).asInstanceOf[Key[util.Map[File, util.Collection[File]]]]
-  }
-
-  private def invalidateBoundForms(source: File): Unit = {
-    FormsToCompileKey.foreach { key =>
-      val boundForms: Option[Iterable[File]] = {
-        val target = sourceToTarget(source.toPath).getOrElse(chunk.representativeTarget())
-        //noinspection ApiStatus
-        val sourceToForm = context.getProjectDescriptor.dataManager.getSourceToFormMap(target)
-        val sourcePath = FileUtil.toCanonicalPath(source.getPath)
-        Option(sourceToForm.getOutputs(sourcePath)).map(_.asScala.map(new File(_)))
-      }
-
-      boundForms.foreach { forms =>
-        val formsToCompile = Option(key.get(context)).getOrElse(new util.HashMap[File, util.Collection[File]]())
-        formsToCompile.put(source, forms.toVector.asJava)
-        key.set(context, formsToCompile)
-      }
-    }
   }
 }
