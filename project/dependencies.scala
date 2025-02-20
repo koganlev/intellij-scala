0759e3b875166de81a25b470410774e70fbc4229
Timur Aliberdov <timur.aliberdov@jetbrains.com>
2025-02-20 19:33:41 +0100

    Update intellijVersion to 251.22821.38, fix compilation

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,7 +15,7 @@ object Versions {
   val zincVersion = "1.10.7"
 
   // ATTENTION: check the comment in `Common.newProjectWithKotlin` when updating this version
-  val intellijVersion = "251.22821.18"
+  val intellijVersion = "251.22821.38"
 
   def isNightlyIntellijVersion: Boolean = intellijVersion.count(_ == '.') == 1
 
--- scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchTooltipsHandler.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/annotator/TypeMismatchTooltipsHandler.scala
@@ -12,6 +12,8 @@ import org.jetbrains.plugins.scala.project.ProjectExt
 import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 import org.jetbrains.plugins.scala.startup.ProjectActivity
 
+import scala.annotation.nowarn
+
 // Annotation.setTooltip is shown both on mouse hover (EditorMouseHoverPopupManager), and on Ctrl / Cmd + F1 (ShowErrorDescriptionAction)
 // But when type mismatch hints are enabled, we don't want tooltips on mouse hover (on the expression), but still want tooltips on the explicit action.
 // https://youtrack.jetbrains.com/issue/SCL-15782
@@ -37,6 +39,7 @@ private object TypeMismatchTooltipsHandler {
           val position = editor.xyToLogicalPosition(point)
           val offset = editor.logicalPositionToOffset(position)
 
+          @nowarn("cat=deprecation") // TODO: IJPL-339
           val highlightInfo = Option(DaemonCodeAnalyzer.getInstance(project).asInstanceOf[DaemonCodeAnalyzerImpl]
             .findHighlightByOffset(editor.getDocument, offset, false))
 
