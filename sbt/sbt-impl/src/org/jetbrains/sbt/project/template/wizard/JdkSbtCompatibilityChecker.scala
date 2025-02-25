d92aae676ed913cdad4bd680fbfdced9a98da0f2
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-02-25 12:10:32 +0100

    NPW: display warnings if potentially incompatible sbt and JDK versions are selected #SCL-23384 fixed

---
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/template/wizard/JdkSbtCompatibilityChecker.scala
@@ -0,0 +1,56 @@
+package org.jetbrains.sbt.project.template.wizard
+
+import com.intellij.util.lang.JavaVersion
+
+import scala.math.Ordered.orderingToOrdered
+import org.jetbrains.plugins.scala.project.Version
+
+object JdkSbtCompatibilityChecker {
+  /**
+   * It's a hardcoded minimum working versions table from [[https://docs.scala-lang.org/overviews/jdk-compatibility/overview.html#tooling-compatibility-table]] <br>
+   * In the future, this table should be automatically updated, for example, by a scheduled CI job.
+   */
+  private val compatibilityTable: Map[JavaVersion, Version] = Map(
+    JavaVersion.compose(23) -> Version("1.9.0"),
+    JavaVersion.compose(21) -> Version("1.9.0"),
+    JavaVersion.compose(17) -> Version("1.6.0"),
+    JavaVersion.compose(11) -> Version("1.1.0"),
+    JavaVersion.compose(8)  -> Version("1.0.0")
+  )
+
+  /**
+   * Determines the minimum compatible sbt version required for a specific JDK version.
+   * It's done based on hardcoded [[org.jetbrains.sbt.project.template.wizard.JdkSbtCompatibilityChecker.compatibilityTable]].
+   * If the JDK is not listed in the table (it is either below 1.8 or greater than or equal to 24"), then the versions are considered as compatible.
+   *
+   * @return <code>Option</code> containing the minimum required sbt version if the given combination of JDK and sbt is incompatible. <br>
+   *         <code>None</code> if the provided versions are compatible.
+   */
+  def getMinimumSbtToJdkCompatibleVersion(jdk: JavaVersion, sbtVersion: Version): Option[Version] = {
+    if (jdk >= JavaVersion.compose(24)) return None
+
+    val nearestCompatibleJdk = compatibilityTable.keys.filter(_ <= jdk).maxOption
+
+    nearestCompatibleJdk
+      .map(compatibilityTable)
+      .filter(requiredSbtVersion => sbtVersion < requiredSbtVersion)
+  }
+
+  /**
+   * Determines the lowest JDK version that is incompatible with the given sbt version.
+   * It's done based on hardcoded [[org.jetbrains.sbt.project.template.wizard.JdkSbtCompatibilityChecker.compatibilityTable]].
+   *
+   * For example - for sbt version 1.5.0, this method will return JDK 17, meaning that sbt 1.5.0 is only compatible with
+   *    JDK versions lower than 17.
+   *
+   * @return <code>Option</code> containing the lowest incompatible JDK for the given sbt version <br>
+   *         <code>None</code> if the versions are compatible
+   */
+  def getLowestIncompatibleJdkForSbt(jdk: JavaVersion, sbtVersion: Version): Option[JavaVersion] = {
+    val minimumSbtVersion = getMinimumSbtToJdkCompatibleVersion(jdk, sbtVersion)
+    minimumSbtVersion.flatMap { _ =>
+      val higherSbtVersions = compatibilityTable.filter { case (_, v) => sbtVersion < v }
+      higherSbtVersions.keys.minOption
+    }
+  }
+}
--- sbt/sbt-impl/src/org/jetbrains/sbt/project/template/wizard/SbtNewProjectWizardStep.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/template/wizard/SbtNewProjectWizardStep.scala
@@ -1,19 +1,25 @@
 package org.jetbrains.sbt.project.template.wizard
 
 import com.intellij.ide.JavaUiBundle
-import com.intellij.ide.projectWizard.ProjectWizardJdkComboBoxKt.projectWizardJdkComboBox
+import com.intellij.ide.projectWizard.ProjectWizardJdkIntent.DetectedJdk
+import com.intellij.ide.projectWizard.{ProjectWizardJdkComboBox, ProjectWizardJdkComboBoxKt}
 import com.intellij.ide.projectWizard.generators.JdkDownloadService
 import com.intellij.ide.wizard.{AbstractNewProjectWizardStep, NewProjectWizardStep}
 import com.intellij.openapi.Disposable
-import com.intellij.openapi.observable.properties.{GraphProperty, PropertyGraph}
+import com.intellij.openapi.observable.properties.{GraphProperty, ObservableProperty, PropertyGraph}
 import com.intellij.openapi.progress.ProgressIndicator
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.projectRoots.impl.jdkDownloader.JdkDownloadTask
 import com.intellij.openapi.projectRoots.{JavaSdkVersion, Sdk}
 import com.intellij.openapi.roots.ui.configuration.projectRoot.SdkDownloadTask
+import com.intellij.ide.wizard.NewProjectWizardBaseData.getBaseData
+import com.intellij.openapi.ui.ValidationInfo
+import com.intellij.openapi.ui.validation.DialogValidationRequestor
 import com.intellij.ui.components.JBCheckBox
 import com.intellij.ui.dsl.builder.{BottomGap, Panel, Row, RowLayout}
 import com.intellij.ui.dsl.gridLayout.HorizontalAlign
+import com.intellij.util.lang.JavaVersion
+import org.jetbrains.annotations.Nullable
 import org.jetbrains.plugins.scala.extensions._
 import org.jetbrains.plugins.scala.project.{Version, Versions}
 import org.jetbrains.plugins.scala.util.AsynchronousVersionsDownloading
@@ -24,10 +30,12 @@ import org.jetbrains.sbt.project.template.SComboBox
 import java.lang
 import java.util.concurrent.atomic.AtomicBoolean
 import kotlin.Unit.{INSTANCE => KUnit}
+import kotlin.jvm.functions
 import scala.annotation.nowarn
 import scala.collection.immutable.ListSet
 
-//TODO add downloading Scala and its combobox to this step so that everything needed for sbt is in one place
+//TODO Add a Scala combobox (and all related functionality, such as downloading) to this step so that everything needed
+// for sbt is in one place.
 abstract class SbtNewProjectWizardStep(parent: NewProjectWizardStep) extends AbstractNewProjectWizardStep(parent)
   with AsynchronousVersionsDownloading {
 
@@ -56,11 +64,18 @@ abstract class SbtNewProjectWizardStep(parent: NewProjectWizardStep) extends Abs
     _.setToolTipText(SbtBundle.message("sbt.download.sbt.sources"))
   )
 
+  private var jdkComboBox: ProjectWizardJdkComboBox = _
+
   protected def setupSbtUI(panel: Panel): Unit =
     panel.row(SbtBundle.message("sbt.settings.sbt"), (row: Row) => {
       row.layout(RowLayout.PARENT_GRID)
 
       val sbtVersionComboBoxCell = row.cell(sbtVersionComboBox).horizontalAlign(HorizontalAlign.FILL): @nowarn("cat=deprecation")
+      sbtVersionComboBoxCell
+        .validationRequestor(afterPropertyChange(sdkProperty))
+        .validationRequestor(afterPropertyChange(sdkDownloadTaskProperty))
+        .validationRequestor(afterPropertyChange(sbtVersionProperty))
+        .validationOnInput(() => sbtWithJdkValidation())
       val downloadSbtSourcesCheckboxCell = row.cell(downloadSbtSourcesCheckbox)
 
       KotlinInteropUtils.bindItem(sbtVersionComboBoxCell, sbtVersionProperty)
@@ -69,23 +84,87 @@ abstract class SbtNewProjectWizardStep(parent: NewProjectWizardStep) extends Abs
       KUnit
     })
 
+  protected def afterPropertyChange[T](property: GraphProperty[T]): DialogValidationRequestor =
+    new DialogValidationRequestor.WithParameter[ObservableProperty[T]] {
+      override def invoke(observable: ObservableProperty[T]): DialogValidationRequestor = {
+        (disposable: Disposable, validate: functions.Function0[kotlin.Unit]) =>
+          observable.afterChange(disposable, (_: T) => validate.invoke())
+      }
+    }.invoke(property)
+
   protected def startJdkDownloadIfNeeded(project: Project): Unit =
     sdkDownloadTask.collect { case task: JdkDownloadTask =>
       val service = project.getService(classOf[JdkDownloadService])
       service.scheduleDownloadJdkForNewProject(task)
     }
 
-  protected def setupJavaSdkUI(builder: Panel): Unit =
+  protected def setupJavaSdkUI(builder: Panel): Unit = {
     builder.row(JavaUiBundle.message("label.project.wizard.new.project.jdk"), (row: Row) => {
-      projectWizardJdkComboBox(this, row, sdkProperty, sdkDownloadTaskProperty)
+      // TODO If a ProjectWizardJdkPredicate parameter is added to the Kotlin extension function `#projectWizardJdkComboBox`,
+      //  make use of it, as it handles many parameters that are currently duplicated here.
+      val jdkComboBoxCell = ProjectWizardJdkComboBoxKt.projectWizardJdkComboBox(
+        row,
+        sdkProperty,
+        sdkDownloadTaskProperty,
+        getBaseData(this).getPathProperty,
+        { s: Sdk =>
+          getContext.setProjectJdk(s)
+          KUnit
+        },
+        getContext.getDisposable,
+        getContext.getProjectJdk,
+        { _: Sdk => { lang.Boolean.TRUE }},
+        (javaVersion: JavaVersion, _: String) => jdkWithSbtValidation(javaVersion)
+      )
+      jdkComboBoxCell
+        .validationRequestor(afterPropertyChange(sbtVersionProperty))
+        .validationRequestor(afterPropertyChange(sdkProperty))
+        .validationRequestor(afterPropertyChange(sdkDownloadTaskProperty))
+
+      jdkComboBox = jdkComboBoxCell.getComponent
+
       KUnit
     }).bottomGap(BottomGap.SMALL)
+  }
+
+  @Nullable
+  private def jdkWithSbtValidation(javaVersion: JavaVersion): String = {
+    if (javaVersion == null) return null
+    val sbtVersion = sbtVersionProperty.get()
+    val minimumCompatibleVersion = JdkSbtCompatibilityChecker.getMinimumSbtToJdkCompatibleVersion(javaVersion, sbtVersion)
+    minimumCompatibleVersion.map { version =>
+      SbtBundle.message("jdk.sbt.incompatible.versions.message", javaVersion.feature, version.presentation)
+    }.orNull
+  }
+
+  @Nullable
+  private def sbtWithJdkValidation(): ValidationInfo = {
+    val jdkVersion = getExpectedJavaSdkVersion.orNull
+    if (jdkVersion == null) return null
+    val sbtVersion = sbtVersionProperty.get()
+    val javaVersion = JavaVersion.compose(jdkVersion.getMaxLanguageLevel.feature())
+    val minimumCompatibleVersion = JdkSbtCompatibilityChecker.getLowestIncompatibleJdkForSbt(javaVersion, sbtVersion)
+    minimumCompatibleVersion.map { version =>
+      new ValidationInfo(SbtBundle.message("sbt.incompatible.versions.message", sbtVersion.presentation, version.toFeatureString), sbtVersionComboBox).asWarning()
+    }.orNull
+  }
 
   protected def getExpectedJavaSdkVersion: Option[JavaSdkVersion] = {
     val versionString = sdk.map(_.getVersionString)
     val plannedVersion = sdkDownloadTask.map(_.getPlannedVersion)
-    val expectedJdkVersion = versionString.orElse(plannedVersion)
+    val expectedJdkVersion = versionString
+      .orElse(getDetectedJdkIfAny)
+      .orElse(plannedVersion)
     expectedJdkVersion.map(JavaSdkVersion.fromVersionString)
+  }
+
+  /**
+   * It's a workaround for <a href="https://youtrack.jetbrains.com/issue/IDEA-368023/The-sdkProperty-is-null-when-JDK-from-the-detected-JDKs-is-chosen-rather-than-from-the-registered-ones">IDEA-368023</a>.
+   * Remove when it's fixed
+   */
+  private def getDetectedJdkIfAny: Option[String] = {
+    val selectedJdk = Option(jdkComboBox).flatMap(cb => Option(cb.getSelectedItem))
+    selectedJdk.collect { case x: DetectedJdk => x.getVersion }
   }
 
   protected final def downloadSbtVersions(disposable: Disposable): Unit = {
