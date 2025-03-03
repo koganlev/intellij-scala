7142d8c4e183bd9d60e14b275be9bd69a5b06c15
Aleksandra Zdrojowa <aleksandra.zdrojowa@jetbrains.com>
2025-03-03 11:38:44 +0100

    sbt: display the update run configuration notification upon project opening (#SCL-23257, #SCL-23654) fixed
    - Store the information about the configuration update in the persistent component
    - If the project is closed before the indexes are ready, the notification will be displayed on project opening
    - if the notification is displayed but there is no action from the user (opening the dialog window or ignoring the notification), the notification will be displayed again when the project is opened

--- sbt/sbt-impl/src/org/jetbrains/sbt/project/UpdateConfigurationImportListener.scala
+++ sbt/sbt-impl/src/org/jetbrains/sbt/project/UpdateConfigurationImportListener.scala
@@ -10,6 +10,8 @@ import org.jetbrains.plugins.scala.settings.ScalaProjectSettings
 import org.jetbrains.sbt.project.MigrateConfigurationsDialogWrapper.ModuleConfigurationExt
 import org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.{ModuleConfiguration, ModuleHeuristicResult, getConfigurationToHeuristicResult}
 import org.jetbrains.sbt.SbtUtil
+import org.jetbrains.sbt.project.settings.ShouldUpdateRunConfigurations
+
 
 /**
  * Project import listener created to detect whether a notification with upgrade configuration action should be displayed.
@@ -26,33 +28,27 @@ class UpdateConfigurationImportListener(project: Project) extends ProjectDataImp
     val isTrustedProject = TrustedProjects.isTrusted(project)
     if (!(SbtUtil.isSbtProject(project) && isTrustedProject)) return
 
-    // note: we need to wait until the project switches to smart mode before executing
-    // this logic, because under the hood it calls JavaExecutionUtil#findMainClass
-    // (from com.intellij.execution.configurations.JavaRunConfigurationModule.findNotNullClass),
-    // which for a project in a dumb mode returns null so we can get incorrect results.
-    invokeWhenSmart(project) {
-      val newSeparateProdTestSourcesValue = getSeparateProdTestSourcesValue
-      val separateProdTestSourcesChanged = newSeparateProdTestSourcesValue != separateProdTestSources
-      val shouldUpdate = shouldUpdateRunConfigurations(project, separateProdTestSourcesChanged)
-      if (shouldUpdate) {
-        // If separate prod/test sources were enabled before the reload and are disabled now,
-        // it means this feature has been switched off, indicating a downgrade.
-        // For more details check org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.IsDowngradingFromSeparateMainTestModules
-        val isDowngrading = Some(separateProdTestSources && !newSeparateProdTestSourcesValue)
-        val configToHeuristicResult = getConfigurationToHeuristicResult(project, isDowngrading)
-        /*
-        The listener only updates the run configurations in advance and not whenever the user calls `SbtMigrateConfigurationsAction` because:
-         1. The heuristic is less accurate when the user calls this action from all actions - since it's unclear what exactly happened;
-            the user might want to adjust some configurations from the old grouping to the new one, or some configurations might need to be downgraded from main/test modules.
-         2. If there is an issue with the heuristic, the user can call `SbtMigrateConfigurationsAction` explicitly and manually apply changes to the configuration modules.
-         */
-        val notModifiedConfigurations = applyHeuristicResultsIfPossible(configToHeuristicResult)
-        val containsNonTemporaryConfigs = notModifiedConfigurations.exists { case (config, _) => !config.isTemporary }
-        if (containsNonTemporaryConfigs) {
-          showNotification(isDowngrading)
-        }
-      }
-      setNotificationShown()
+    val newSeparateProdTestSourcesValue = getSeparateProdTestSourcesValue
+    val separateProdTestSourcesChanged = newSeparateProdTestSourcesValue != separateProdTestSources
+    val shouldUpdate = shouldUpdateRunConfigurations(project, separateProdTestSourcesChanged)
+
+    // If separate prod/test sources were enabled before the reload and are disabled now,
+    // it means this feature has been switched off, indicating a downgrade.
+    // For more details check org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.IsDowngradingFromSeparateMainTestModules
+    val isDowngrading = separateProdTestSources && !newSeparateProdTestSourcesValue
+
+    // Updating the state before calling #update ensures that if the project is closed before the indexes are ready,
+    // the notification will still be displayed when the project is reopened.
+    ShouldUpdateRunConfigurations.updateState(project, shouldUpdate, isDowngrading)
+
+    if (shouldUpdate) {
+      // If an update is expected, it's better to close all existing notifications first.
+      // This prevents the user from interacting with the outdated notification, which could lead
+      // to displaying many broken configurations in the dialog.
+      // These configurations will likely be automatically updated in the upcoming update method.
+      UpdateRunConfigurationsNotification.closeAllExistingNotifications(project)
+
+      UpdateConfigurationImportListener.update(isDowngrading, project)
     }
   }
 
@@ -74,14 +70,41 @@ class UpdateConfigurationImportListener(project: Project) extends ProjectDataImp
 
     !isNew && (!isMigrateConfigurationsNotificationShown || prodTestSourcesHasChanged)
   }
+}
 
-  private def showNotification(isDowngradingFromSeparateMainTestModules: Option[Boolean]): Unit = {
+object UpdateConfigurationImportListener {
+
+  def update(isDowngrading: Boolean, project: Project): Unit = {
+    // note: we need to wait until the project switches to smart mode before executing
+    // this logic, because under the hood it calls JavaExecutionUtil#findMainClass
+    // (from com.intellij.execution.configurations.JavaRunConfigurationModule.findNotNullClass),
+    // which for a project in a dumb mode returns null so we can get incorrect results.
+    invokeWhenSmart(project) {
+      val configToHeuristicResult = getConfigurationToHeuristicResult(project, Some(isDowngrading))
+      /*
+      The listener only updates the run configurations in advance and not whenever the user calls `SbtMigrateConfigurationsAction` because:
+       1. The heuristic is less accurate when the user calls this action from all actions - since it's unclear what exactly happened;
+          the user might want to adjust some configurations from the old grouping to the new one, or some configurations might need to be downgraded from main/test modules.
+       2. If there is an issue with the heuristic, the user can call `SbtMigrateConfigurationsAction` explicitly and manually apply changes to the configuration modules.
+      */
+      val notModifiedConfigurations = applyHeuristicResultsIfPossible(configToHeuristicResult)
+      val containsNonTemporaryConfigs = notModifiedConfigurations.exists {  case (config, _) => !config.isTemporary }
+      if (containsNonTemporaryConfigs) {
+        showNotification(Some(isDowngrading), project)
+      } else {
+        ShouldUpdateRunConfigurations.getInstance(project).shouldUpdate = false
+      }
+      setMigrateNotificationShown(project)
+    }
+  }
+
+  private def showNotification(isDowngradingFromSeparateMainTestModules: Option[Boolean], project: Project): Unit = {
     UpdateRunConfigurationsNotification.closeAllExistingSuggestions(project)
     val notification = new UpdateRunConfigurationsNotification(project, isDowngradingFromSeparateMainTestModules)
     notification.notify(project)
   }
 
-  private def setNotificationShown(): Unit =
+  private def setMigrateNotificationShown(project: Project): Unit =
     ScalaProjectSettings.getInstance(project).setMigrateConfigurationsNotificationShown(true)
 
   /**
@@ -92,7 +115,7 @@ class UpdateConfigurationImportListener(project: Project) extends ProjectDataImp
   private def applyHeuristicResultsIfPossible(
     configToHeuristicResult: Seq[(ModuleConfiguration, ModuleHeuristicResult)]
   ): Seq[(ModuleConfiguration, ModuleHeuristicResult)] =
-     configToHeuristicResult.filter { case (config, ModuleHeuristicResult(moduleOpt, _)) =>
+    configToHeuristicResult.filter { case (config, ModuleHeuristicResult(moduleOpt, _)) =>
       moduleOpt match {
         case Some(module) =>
           config.setModule(module)
