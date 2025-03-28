ada8c64db26ad0c81642f5912ad51fe711573ce2
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-03-28 15:44:43 +0100

    [scratch worksheet] Further simplify the implementation of WorksheetModuleService #SCL-23723
    - Holding onto strong references to Modules leads to leaked modules and projects (we got "too many projects leaked" exceptions in tests).
    - But given that we do not have any wrappers on top of modules (no synthetic modules exist any more), we also do not need to populate an association map.
    - Even if we do, we still need to compute a possibly new representative module (in case the module was changed), so additionally caching the result brings no value.
    - I've also replaced the custom strong reference implementation with a weak reference. That way, the lifecycle of scratch worksheet VirtualFiles does not need to affect the lifecycle of the modules they are contained within.

--- scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/WorksheetModuleService.scala
+++ scala/worksheet/src/org/jetbrains/plugins/scala/worksheet/actions/WorksheetModuleService.scala
@@ -8,20 +8,17 @@ import com.intellij.util.SlowOperations
 import org.jetbrains.plugins.scala.project.{ModuleExt, UserDataKeys}
 import org.jetbrains.plugins.scala.worksheet.settings.WorksheetFileSettings
 
-import scala.collection.mutable
-import scala.ref.Reference
+import scala.ref.WeakReference
 import scala.util.Using
 
 @Service(Array(Service.Level.PROJECT))
 final class WorksheetModuleService(project: Project) {
 
-  private val modulesMap = mutable.HashMap[VirtualFile, Module]()
-
   def ensureModuleAttached(virtualFile: VirtualFile): Unit = {
     for {
       wrapperModule <- moduleForFile(virtualFile)
     } {
-      val moduleReferenceRef = moduleReference(wrapperModule)
+      val moduleReferenceRef = WeakReference(wrapperModule)
       virtualFile.putUserData(UserDataKeys.SCALA_ATTACHED_MODULE, moduleReferenceRef)
     }
   }
@@ -31,34 +28,8 @@ final class WorksheetModuleService(project: Project) {
     val cpModule = Using.resource(SlowOperations.knownIssue("SCL-22095, SCL-22097")) { _ =>
       WorksheetFileSettings(project, virtualFile).getModule
     }
-    cpModule.map(moduleForFile(virtualFile, _))
+    cpModule.map(_.findRepresentativeModuleForSharedSourceModuleOrSelf)
   }
-
-  private def moduleForFile(virtualFile: VirtualFile, currentCpModule: Module): Module = {
-    val maybeCached = modulesMap.get(virtualFile)
-    val representative = currentCpModule.findRepresentativeModuleForSharedSourceModuleOrSelf
-    val result = maybeCached match {
-      case Some(cached) =>
-        if (cached != representative) {
-          modulesMap.put(virtualFile, representative)
-          representative
-        } else
-          cached
-      case _ =>
-        modulesMap.put(virtualFile, representative)
-        representative
-    }
-    result
-  }
-
-  private def moduleReference(module: Module): Reference[Module] =
-    new Reference[Module] {
-      override def apply(): Module = module
-      override def get: Option[Module] = Some(apply())
-      override def clear(): Unit = ()
-      override def enqueue(): Boolean = false
-      override def isEnqueued: Boolean = false
-    }
 }
 
 object WorksheetModuleService {
