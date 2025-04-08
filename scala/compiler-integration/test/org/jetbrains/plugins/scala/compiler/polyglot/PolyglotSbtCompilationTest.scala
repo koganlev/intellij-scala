3227f4a3de2f2736df52d4cf1246e29e76b44c2d
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-04-08 16:53:36 +0200

    [tests] Make PolyglotSbtCompilationTest extends SbtProjectCompilationTestBase

--- scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/polyglot/PolyglotSbtCompilationTest.scala
+++ scala/compiler-integration/test/org/jetbrains/plugins/scala/compiler/polyglot/PolyglotSbtCompilationTest.scala
@@ -1,61 +1,27 @@
 package org.jetbrains.plugins.scala.compiler.polyglot
 
-import com.intellij.openapi.externalSystem.model.ProjectSystemId
 import com.intellij.openapi.module.{Module, ModuleManager}
-import com.intellij.openapi.projectRoots.{ProjectJdkTable, Sdk}
+import com.intellij.openapi.projectRoots.ProjectJdkTable
 import com.intellij.openapi.roots.ModuleRootModificationUtil
-import com.intellij.platform.externalSystem.testFramework.ExternalSystemImportingTestCase
 import com.intellij.testFramework.{CompilerTester, IndexingTestUtil}
 import junit.framework.TestCase.{assertEquals, assertNotNull}
 import org.jetbrains.plugins.scala.CompilationTests_Zinc
-import org.jetbrains.plugins.scala.base.libraryLoaders.SmartJDKLoader
+import org.jetbrains.plugins.scala.compiler.SbtProjectCompilationTestBase
 import org.jetbrains.plugins.scala.compiler.data.IncrementalityType
-import org.jetbrains.plugins.scala.compiler.{CompileServerLauncher, JdkVersionDiscovery}
 import org.jetbrains.plugins.scala.extensions.inWriteAction
 import org.jetbrains.plugins.scala.project.settings.ScalaCompilerConfiguration
-import org.jetbrains.plugins.scala.settings.ScalaCompileServerSettings
-import org.jetbrains.sbt.Sbt
-import org.jetbrains.sbt.project.settings.SbtProjectSettings
-import org.jetbrains.sbt.project.{SbtCachesSetupUtil, SbtProjectSystem}
 import org.junit.experimental.categories.Category
 
 @Category(Array(classOf[CompilationTests_Zinc]))
-abstract class PolyglotSbtCompilationTestBase(separateModules: Boolean) extends ExternalSystemImportingTestCase {
-
-  private var sdk: Sdk = _
-
-  private var compiler: CompilerTester = _
+abstract class PolyglotSbtCompilationTestBase(separateModules: Boolean)
+  extends SbtProjectCompilationTestBase(separateProdAndTestSources = separateModules) {
 
   private var module1: Module = _
 
   private var module2: Module = _
 
-  override lazy val getCurrentExternalProjectSettings: SbtProjectSettings = {
-    val settings = new SbtProjectSettings()
-    settings.separateProdAndTestSources = separateModules
-    settings.jdk = sdk.getName
-    settings
-  }
-
-  override def getExternalSystemId: ProjectSystemId = SbtProjectSystem.Id
-
-  override def getTestsTempDir: String = this.getClass.getSimpleName
-
-  override def getExternalSystemConfigFileName: String = Sbt.BuildFile
-
   override def setUp(): Unit = {
     super.setUp()
-
-    sdk = {
-      val jdkVersion = JdkVersionDiscovery.discoveredJdk
-      val res = SmartJDKLoader.getOrCreateJDK(jdkVersion)
-      val settings = ScalaCompileServerSettings.getInstance()
-      settings.COMPILE_SERVER_SDK = res.getName
-      settings.USE_DEFAULT_SDK = false
-      res
-    }
-
-    SbtCachesSetupUtil.setupCoursierAndIvyCache(getProject)
 
     createProjectSubDirs("project", "module1/src/main/java", "module1/src/main/kotlin", "module2/src/main/scala")
     createProjectSubFile("project/build.properties",
@@ -94,7 +60,7 @@ abstract class PolyglotSbtCompilationTestBase(separateModules: Boolean) extends 
       """object HelloWorldGreeter extends AbstractGreeter("Hello, world!")
         |""".stripMargin)
 
-    importProject()
+    importProject(false)
 
     KotlinDaemonUtil.disableKotlinDaemon(getProject)
 
@@ -114,14 +80,8 @@ abstract class PolyglotSbtCompilationTestBase(separateModules: Boolean) extends 
   }
 
   override def tearDown(): Unit = try {
-    CompileServerLauncher.stopServerAndWait()
-    compiler.tearDown()
-    val settings = ScalaCompileServerSettings.getInstance()
-    settings.USE_DEFAULT_SDK = true
-    settings.COMPILE_SERVER_SDK = null
     inWriteAction {
       val jdkTable = ProjectJdkTable.getInstance()
-      jdkTable.removeJdk(sdk)
       val kotlinSdk = jdkTable.getAllJdks.find(_.getName.contains("Kotlin SDK"))
       kotlinSdk.foreach(jdkTable.removeJdk)
     }
