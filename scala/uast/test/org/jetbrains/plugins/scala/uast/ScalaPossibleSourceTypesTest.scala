2e314693002e2477a211e7121de4d85a608d1e15
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-05-06 23:45:59 +0200

    [tests] Rewrite ScalaPossibleSourceTypesTest in terms of the new JUnit 4 parameterized file set tests

--- scala/uast/test/org/jetbrains/plugins/scala/uast/ScalaPossibleSourceTypesTest.scala
+++ scala/uast/test/org/jetbrains/plugins/scala/uast/ScalaPossibleSourceTypesTest.scala
@@ -1,38 +1,31 @@
 package org.jetbrains.plugins.scala.uast
 
-import com.intellij.lang.Language
-import com.intellij.openapi.project.Project
 import com.intellij.platform.uast.testFramework.common.AllUastTypesKt.allUElementSubtypes
 import com.intellij.platform.uast.testFramework.common.PossibleSourceTypesTestBase
-import junit.framework.{Test, TestCase}
-import org.jetbrains.plugins.scala.base.ScalaFileSetTestCase
+import org.jetbrains.plugins.scala.base.{DefaultFileSetTestTransform, NoSdkFileSetTestBase}
 import org.jetbrains.plugins.scala.lang.psi.uast.withPossibleSourceTypesCheck
-import org.jetbrains.plugins.scala.{ScalaLanguage, ScalaVersion}
-import org.jetbrains.uast._
+import org.jetbrains.uast.{UCallableReferenceExpression, UClass, UField, UFile, UMethod, UQualifiedReferenceExpression, USimpleNameReferenceExpression, UastFacade}
 
-import scala.jdk.CollectionConverters.IterableHasAsScala
+import java.nio.file.Path
+import scala.jdk.CollectionConverters._
 
-class ScalaPossibleSourceTypesTest extends TestCase
+class ScalaPossibleSourceTypesTest extends NoSdkFileSetTestBase with DefaultFileSetTestTransform with PossibleSourceTypesTestBase {
+  override protected def relativeTestDataPath: Path = Path.of("parser", "data")
 
-object ScalaPossibleSourceTypesTest {
-  def suite(): Test = new ScalaFileSetTestCase("/parser/data") with PossibleSourceTypesTestBase {
-    override protected def getLanguage: Language = ScalaLanguage.INSTANCE
+  override protected def runTest(testName: String, fileText: String): Unit = withPossibleSourceTypesCheck {
+    val file = createLightFile(fileText)
+    val uFile = UastFacade.INSTANCE.convertElementWithParent[UFile](file, Array.empty)
 
-    override protected def runTest(testName0: String, content: String, project: Project): Unit = withPossibleSourceTypesCheck {
-      val file = createLightFile(content, project)
-      val uFile = UastFacade.INSTANCE.convertElementWithParent[UFile](file, Array())
-
-      val psiFile = uFile.getSourcePsi
-      for (uastType <- allUElementSubtypes.asScala) {
-        checkConsistencyWithRequiredTypes(psiFile, uastType)
-      }
-      checkConsistencyWithRequiredTypes(psiFile, classOf[UClass], classOf[UMethod], classOf[UField])
-      checkConsistencyWithRequiredTypes(
-        psiFile,
-        classOf[USimpleNameReferenceExpression],
-        classOf[UQualifiedReferenceExpression],
-        classOf[UCallableReferenceExpression]
-      )
+    val psiFile = uFile.getSourcePsi
+    for (uastType <- allUElementSubtypes.asScala) {
+      checkConsistencyWithRequiredTypes(psiFile, uastType)
     }
+    checkConsistencyWithRequiredTypes(psiFile, classOf[UClass], classOf[UMethod], classOf[UField])
+    checkConsistencyWithRequiredTypes(
+      psiFile,
+      classOf[USimpleNameReferenceExpression],
+      classOf[UQualifiedReferenceExpression],
+      classOf[UCallableReferenceExpression]
+    )
   }
 }
