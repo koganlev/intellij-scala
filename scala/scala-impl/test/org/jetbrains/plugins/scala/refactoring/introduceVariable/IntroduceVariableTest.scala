5f411d3bc73db320510b650ae23547c498545f25
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-05-07 15:09:09 +0200

    [tests] Rewrite IntroduceVariableTest in terms of the new JUnit 4 parameterized file set tests

--- scala/scala-impl/test/org/jetbrains/plugins/scala/refactoring/introduceVariable/IntroduceVariableTest.scala
+++ scala/scala-impl/test/org/jetbrains/plugins/scala/refactoring/introduceVariable/IntroduceVariableTest.scala
@@ -2,21 +2,47 @@ package org.jetbrains.plugins.scala
 package refactoring
 package introduceVariable
 
-import junit.framework.{Test, TestCase}
+import com.intellij.lang.Language
+import org.jetbrains.plugins.scala.base.SdkFileSetTestBase
+import org.jetbrains.plugins.scala.lang.actions.NewActionTestBase
+import org.jetbrains.plugins.scala.lang.formatting.settings.ScalaCodeStyleSettings
+import org.jetbrains.plugins.scala.util.TypeAnnotationSettings
 
-class IntroduceVariableTest extends TestCase
+import java.nio.file.Path
 
-object IntroduceVariableTest {
-  private val DATA_PATH = "/refactoring/introduceVariable/data"
-
-  def suite(): Test = new IntroduceVariableTestSuite(DATA_PATH, ScalaLanguage.INSTANCE)
+class IntroduceVariableTest extends IntroduceVariableTestBase {
+  override protected def relativeTestDataPath: Path = Path.of("refactoring", "introduceVariable", "data")
 }
 
+class IntroduceVariableScala3Test extends IntroduceVariableTestBase {
+  override protected def relativeTestDataPath: Path = Path.of("refactoring", "introduceVariable", "data3")
 
-class IntroduceVariableScala3Test extends TestCase
+  override protected def language: Language = Scala3Language.INSTANCE
+}
 
-object IntroduceVariableScala3Test {
-  private val DATA_PATH = "/refactoring/introduceVariable/data3"
+abstract class IntroduceVariableTestBase extends SdkFileSetTestBase with NewActionTestBase {
+  private var fixture: ScalaIntroduceVariableTestFixture = _
 
-  def suite(): Test = new IntroduceVariableTestSuite(DATA_PATH, Scala3Language.INSTANCE)
+  override protected def setUp(): Unit = {
+    super.setUp()
+
+    val alwaysAddTypeSettings = TypeAnnotationSettings.alwaysAddType(ScalaCodeStyleSettings.getInstance(project))
+    fixture = new ScalaIntroduceVariableTestFixture(project, Some(alwaysAddTypeSettings), language)
+    fixture.setUp()
+  }
+
+  override protected def tearDown(): Unit = {
+    fixture.tearDown()
+    super.tearDown()
+  }
+
+  override protected def transform(testName: String, fileText: String): String = {
+    val (extractedText, options) = IntroduceVariableUtils.extractNameFromLeadingComment(fileText)
+    val optionsAdjusted = options.copy(definitionName = options.definitionName.orElse(Some("value")))
+    fixture.configureFromText(extractedText)
+    fixture.invokeIntroduceVariableActionAndGetResult(optionsAdjusted) match {
+      case Left(error) => error
+      case Right(fileTextNew) => fileTextNew
+    }
+  }
 }
