40bfd69b5d5653d6c5a4730f4278d66be35f3f06
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-05-09 10:46:23 +0200

    [tests] Migrate the SbtMigrateConfigurationsActionTest back to JUnit 4 parameterized test style

--- sbt/sbt-impl/test/org/jetbrains/sbt/project/SbtMigrateConfigurationsActionTest.scala
+++ sbt/sbt-impl/test/org/jetbrains/sbt/project/SbtMigrateConfigurationsActionTest.scala
@@ -1,13 +1,14 @@
 package org.jetbrains.sbt.project
 
+import junitparams.{JUnitParamsRunner, Parameters}
 import org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.ConfigDetails
-import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Nested
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
 
-import java.util.stream.Stream
-import org.junit.jupiter.params.ParameterizedTest
-import org.junit.jupiter.params.provider.{Arguments, MethodSource}
+import scala.annotation.unused
 
+@RunWith(classOf[JUnitParamsRunner])
 class SbtMigrateConfigurationsActionTest {
 
   // If separate modules for prod/test are enabled, then only main/test are passed to SbtMigrateConfigurationsAction#doFindModulesForConfig
@@ -21,14 +22,33 @@ class SbtMigrateConfigurationsActionTest {
 
   private val moduleNames_ProdTestSourcesDisabled = Seq("root", "root.foo", "root.foo.dummy", "root~1", "root~1.foo")
 
+  @unused("used reflectively by the @Parameters annotation")
+  private def moduleNameAndMainClassHeuristicsConsistencyTestParameters: Array[AnyRef] = Array(
+    // modules found based on the name -> root.foo.main, root.foo.test, root~1.foo.main, root~1.foo.test
+    // modules with the main class -> root~1.foo.test
+    Array(ConfigDetails(isTest = false, "foo", Some("Main1")), Seq("root~1.foo.test")),
+    // modules found based on the name -> empty
+    // modules with the main class -> root.foo.dummy.test
+    Array(ConfigDetails(isTest = false, "notKnownName", Some("Main3")), Seq("root.foo.dummy.test")),
+    // modules found based on the name -> empty
+    // modules with the main class -> root~1.main, root~1.test
+    Array(ConfigDetails(isTest = false, "root.foo.main", Some("Main4")), Seq("root~1.main", "root~1.test")),
+    // modules found based on the name -> empty
+    // modules with the main class -> empty
+    Array(ConfigDetails(isTest = false, "notKnownName", Some("Alone")), Seq()),
+    // modules found based on the name -> root.foo.main, root.foo.test
+    // modules with the main class -> root~1.main, root~1.test
+    Array(ConfigDetails(isTest = false, "root.foo", Some("Main4")), Seq("root~1.main", "root~1.test", "root.foo.main", "root.foo.test"))
+  )
+
   /**
    * Test the combination of two heuristic results:
    *  1. Finding suitable modules based on their names.
    *  1. Finding suitable modules based on the existence of main classes.
    */
-  @ParameterizedTest
-  @MethodSource(Array("org.jetbrains.sbt.project.SbtMigrateConfigurationsActionTest#moduleNameAndMainClassHeuristicsConsistency"))
-  def testModuleNameAndMainClassHeuristicsConsistency(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit = {
+  @Test
+  @Parameters(method = "moduleNameAndMainClassHeuristicsConsistencyTestParameters")
+  def moduleNameAndMainClassHeuristicsConsistencyTest(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit = {
     val moduleNameToClassesInside = Map (
       "root~1.foo.test" -> Seq("Main1"),
       "root.foo.dummy.test" -> Seq("Main3"),
@@ -45,59 +65,93 @@ class SbtMigrateConfigurationsActionTest {
     )
   }
 
-  /**
-   * Tests inside this class are focused on testing [[org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.isConfigurationCompatibleWithModuleName]],
+  /*
+   * The following tests are focused on testing `org.jetbrains.sbt.project.SbtMigrateConfigurationsAction.isConfigurationCompatibleWithModuleName`,
    * which is responsible for finding suitable modules based on their names.
    */
-  @Nested
-  class FindingSuitableModulesBasedOnTheirNamesTest {
 
-    @ParameterizedTest
-    @MethodSource(Array("org.jetbrains.sbt.project.SbtMigrateConfigurationsActionTest#prodTestEnabled_isDowngradingNoneOrFalse"))
-    def testSeparateModulesForProdTestEnabled_isDowngradingNoneOrFalse(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
-      Seq(None, Some(false)).foreach { isDowngrading =>
-        execute(
-          configDetails,
-          moduleNames_ProdTestSourcesEnabled,
-          prodTestSourcesSeparated = true,
-          isDowngradingFromSeparateMainTestModules = isDowngrading,
-          expectedResult
-        )
-      }
+  @unused("used reflectively by the @Parameters annotation")
+  private def separateModulesForProdTestEnabled_isDowngradingNoneOrFalseTestParameters: Array[AnyRef] = Array(
+    Array(ConfigDetails(isTest = false, "foo", Some("Main1")), Seq("root.foo.main", "root.foo.test", "root~1.foo.main", "root~1.foo.test")),
+    Array(ConfigDetails(isTest = true, "foo", Some("Main1")), Seq("root.foo.test", "root~1.foo.test")),
+    Array(ConfigDetails(isTest = true, "foo", Some("Main1")), Seq("root.foo.test", "root~1.foo.test")),
+    Array(ConfigDetails(isTest = false, "notKnownName", Some("Main1")), Seq()),
+    Array(ConfigDetails(isTest = false, "root.foo", Some("Main4")), Seq("root.foo.main", "root.foo.test")),
+    Array(ConfigDetails(isTest = false, "root.foo.main", None), Seq())
+  )
 
-    @ParameterizedTest
-    @MethodSource(Array("org.jetbrains.sbt.project.SbtMigrateConfigurationsActionTest#prodTestDisabled_isDowngradingTrue"))
-    def testSeparateModulesForProdTestDisabled_isDowngradingTrue(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
+  @Test
+  @Parameters(method = "separateModulesForProdTestEnabled_isDowngradingNoneOrFalseTestParameters")
+  def separateModulesForProdTestEnabled_isDowngradingNoneOrFalseTest(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
+    Seq(None, Some(false)).foreach { isDowngrading =>
       execute(
         configDetails,
-        moduleNames_ProdTestSourcesDisabled,
-        prodTestSourcesSeparated = false,
-        isDowngradingFromSeparateMainTestModules = Some(true),
+        moduleNames_ProdTestSourcesEnabled,
+        prodTestSourcesSeparated = true,
+        isDowngradingFromSeparateMainTestModules = isDowngrading,
         expectedResult
       )
+    }
 
-    @ParameterizedTest
-    @MethodSource(Array("org.jetbrains.sbt.project.SbtMigrateConfigurationsActionTest#prodTestDisabled_isDowngradingFalse"))
-    def testSeparateModulesForProdTestDisabled_isDowngradingFalse(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
-      execute(
-        configDetails,
-        moduleNames_ProdTestSourcesDisabled,
-        prodTestSourcesSeparated = false,
-        isDowngradingFromSeparateMainTestModules = Some(false),
-        expectedResult
-      )
+  @unused("used reflectively by the @Parameters annotation")
+  // downgrading from separate main/test modules
+  private def separateModulesForProdTestDisabled_isDowngradingTrueTestParameters: Array[AnyRef] = Array(
+    Array(ConfigDetails(isTest = false, "root.foo.main", Some("Main")), Seq("root.foo")),
+    Array(ConfigDetails(isTest = false, "root.foo.something", Some("Main")), Seq()),
+    Array(ConfigDetails(isTest = false, "foo", Some("Main")), Seq()),
+    Array(ConfigDetails(isTest = true, "foo.main", Some("Main")), Seq())
+  )
 
-    @ParameterizedTest
-    @MethodSource(Array("org.jetbrains.sbt.project.SbtMigrateConfigurationsActionTest#prodTestDisabled_isDowngradingNone"))
-    def testSeparateModulesForProdTestDisabled_isDowngradingNone(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
-      execute(
-        configDetails,
-        moduleNames_ProdTestSourcesDisabled,
-        prodTestSourcesSeparated = false,
-        isDowngradingFromSeparateMainTestModules = None,
-        expectedResult
-      )
-  }
+  @Test
+  @Parameters(method = "separateModulesForProdTestDisabled_isDowngradingTrueTestParameters")
+  def separateModulesForProdTestDisabled_isDowngradingTrueTest(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
+    execute(
+      configDetails,
+      moduleNames_ProdTestSourcesDisabled,
+      prodTestSourcesSeparated = false,
+      isDowngradingFromSeparateMainTestModules = Some(true),
+      expectedResult
+    )
+
+  @unused("used reflectively by the @Parameters annotation")
+  //upgrading from the old grouping scheme to the new one
+  private def separateModulesForProdTestDisabled_isDowngradingFalseTestParameters: Array[AnyRef] = Array(
+    Array(ConfigDetails(isTest = true, "root.foo.main", None), Seq()),
+    Array(ConfigDetails(isTest = false, "foo", Some("Main")), Seq("root.foo", "root~1.foo")),
+    Array(ConfigDetails(isTest = false, "foo.dummy", Some("Main")), Seq("root.foo.dummy"))
+  )
+  
+  @Test
+  @Parameters(method = "separateModulesForProdTestDisabled_isDowngradingFalseTestParameters")
+  def separateModulesForProdTestDisabled_isDowngradingFalseTest(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
+    execute(
+      configDetails,
+      moduleNames_ProdTestSourcesDisabled,
+      prodTestSourcesSeparated = false,
+      isDowngradingFromSeparateMainTestModules = Some(false),
+      expectedResult
+    )
+    
+  @unused("used reflectively by the @Parameters annotation")
+  // It's unclear what occurred - the user called the action from all actions.
+  // This might be upgrading from the old grouping to the new grouping,
+  // or downgrading from separate modules for prod/test.
+  private def separateModulesForProdTestDisabled_isDowngradingNoneTestParameters: Array[AnyRef] = Array(
+    Array(ConfigDetails(isTest = false, "root.foo.main", Some("Main")), Seq("root.foo")),
+    Array(ConfigDetails(isTest = false, "foo", Some("Main")), Seq("root.foo", "root~1.foo")),
+    Array(ConfigDetails(isTest = true, "foo.main", Some("Main")), Seq())
+  )
+  
+  @Test
+  @Parameters(method = "separateModulesForProdTestDisabled_isDowngradingNoneTestParameters")
+  def separateModulesForProdTestDisabled_isDowngradingNoneTest(configDetails: ConfigDetails, expectedResult: Seq[String]): Unit =
+    execute(
+      configDetails,
+      moduleNames_ProdTestSourcesDisabled,
+      prodTestSourcesSeparated = false,
+      isDowngradingFromSeparateMainTestModules = None,
+      expectedResult
+    )
 
   private def execute(
     configDetails: ConfigDetails,
@@ -129,58 +183,4 @@ class SbtMigrateConfigurationsActionTest {
     )
     assertEquals(expectedResult.sorted, result.sorted)
   }
-}
-
-object SbtMigrateConfigurationsActionTest {
-
-  def moduleNameAndMainClassHeuristicsConsistency(): Stream[Arguments] = Stream.of(
-    // modules found based on the name -> root.foo.main, root.foo.test, root~1.foo.main, root~1.foo.test
-    // modules with the main class -> root~1.foo.test
-    Arguments.of(ConfigDetails(isTest = false, "foo", Some("Main1")), Seq("root~1.foo.test")),
-    // modules found based on the name -> empty
-    // modules with the main class -> root.foo.dummy.test
-    Arguments.of(ConfigDetails(isTest = false, "notKnownName", Some("Main3")), Seq("root.foo.dummy.test")),
-    // modules found based on the name -> empty
-    // modules with the main class -> root~1.main, root~1.test
-    Arguments.of(ConfigDetails(isTest = false, "root.foo.main", Some("Main4")), Seq("root~1.main", "root~1.test")),
-    // modules found based on the name -> empty
-    // modules with the main class -> empty
-    Arguments.of(ConfigDetails(isTest = false, "notKnownName", Some("Alone")), Seq()),
-    // modules found based on the name -> root.foo.main, root.foo.test
-    // modules with the main class -> root~1.main, root~1.test
-    Arguments.of(ConfigDetails(isTest = false, "root.foo", Some("Main4")), Seq("root~1.main", "root~1.test", "root.foo.main", "root.foo.test"))
-  )
-
-  def prodTestEnabled_isDowngradingNoneOrFalse(): Stream[Arguments] = Stream.of(
-    Arguments.of(ConfigDetails(isTest = false, "foo", Some("Main1")), Seq("root.foo.main", "root.foo.test", "root~1.foo.main", "root~1.foo.test")),
-    Arguments.of(ConfigDetails(isTest = true, "foo", Some("Main1")), Seq("root.foo.test", "root~1.foo.test")),
-    Arguments.of(ConfigDetails(isTest = true, "foo", Some("Main1")), Seq("root.foo.test", "root~1.foo.test")),
-    Arguments.of(ConfigDetails(isTest = false, "notKnownName", Some("Main1")), Seq()),
-    Arguments.of(ConfigDetails(isTest = false, "root.foo", Some("Main4")), Seq("root.foo.main", "root.foo.test")),
-    Arguments.of(ConfigDetails(isTest = false, "root.foo.main", None), Seq()),
-  )
-
-  // downgrading from separate main/test modules
-  def prodTestDisabled_isDowngradingTrue(): Stream[Arguments] = Stream.of(
-    Arguments.of(ConfigDetails(isTest = false, "root.foo.main", Some("Main")), Seq("root.foo")),
-    Arguments.of(ConfigDetails(isTest = false, "root.foo.something", Some("Main")), Seq()),
-    Arguments.of(ConfigDetails(isTest = false, "foo", Some("Main")), Seq()),
-    Arguments.of(ConfigDetails(isTest = true, "foo.main", Some("Main")), Seq())
-  )
-
-  //upgrading from the old grouping scheme to the new one
-  def prodTestDisabled_isDowngradingFalse(): Stream[Arguments] = Stream.of(
-    Arguments.of(ConfigDetails(isTest = true, "root.foo.main", None), Seq()),
-    Arguments.of(ConfigDetails(isTest = false, "foo", Some("Main")), Seq("root.foo", "root~1.foo")),
-    Arguments.of(ConfigDetails(isTest = false, "foo.dummy", Some("Main")), Seq("root.foo.dummy"))
-  )
-
-  // It's unclear what occurred - the user called the action from all actions.
-  // This might be upgrading from the old grouping to the new grouping,
-  // or downgrading from separate modules for prod/test.
-  def prodTestDisabled_isDowngradingNone(): Stream[Arguments] = Stream.of(
-    Arguments.of(ConfigDetails(isTest = false, "root.foo.main", Some("Main")), Seq("root.foo")),
-    Arguments.of(ConfigDetails(isTest = false, "foo", Some("Main")), Seq("root.foo", "root~1.foo")),
-    Arguments.of(ConfigDetails(isTest = true, "foo.main", Some("Main")), Seq())
-  )
 }
