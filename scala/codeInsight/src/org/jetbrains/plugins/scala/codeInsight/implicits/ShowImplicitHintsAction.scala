3e4fc34f88dcdbea83609cc167be6c4e0504733b
Vasil Vasilev <vasil.vasilev@jetbrains.com>
2025-05-20 22:49:53 +0200

    [run once config] Refactor ShowImplicitHintsAction$ConfigureShortcuts away from RunOnceStartupActivity to a combination of a ProjectActivity and an application-level service #SCL-23912

--- scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ShowImplicitHintsAction.scala
+++ scala/codeInsight/src/org/jetbrains/plugins/scala/codeInsight/implicits/ShowImplicitHintsAction.scala
@@ -1,12 +1,7 @@
 package org.jetbrains.plugins.scala.codeInsight.implicits
 
 import com.intellij.openapi.actionSystem._
-import com.intellij.openapi.client.ClientSystemInfo
-import com.intellij.openapi.keymap.KeymapManager
 import org.jetbrains.plugins.scala.codeInsight.ScalaCodeInsightBundle
-import org.jetbrains.plugins.scala.components.RunOnceStartupActivity
-
-import javax.swing.KeyStroke
 
 class ShowImplicitHintsAction extends ToggleAction(
   ScalaCodeInsightBundle.message("show.implicit.hints.action.text"),
@@ -29,19 +24,4 @@ class ShowImplicitHintsAction extends ToggleAction(
 
 private object ShowImplicitHintsAction {
   val Id = "Scala.ShowImplicits"
-
-  class ConfigureShortcuts extends RunOnceStartupActivity {
-    override protected def doRunActivity(): Unit = {
-      if (!ClientSystemInfo.isWindows && !ClientSystemInfo.isMac) { // Workaround for SCL-21346
-        val keymap = KeymapManager.getInstance.getActiveKeymap
-        keymap.removeShortcut("ZoomInIdeAction", new KeyboardShortcut(KeyStroke.getKeyStroke("shift control alt EQUALS"), null))
-        keymap.removeShortcut("ZoomOutIdeAction", new KeyboardShortcut(KeyStroke.getKeyStroke("shift control alt MINUS"), null))
-        keymap.removeShortcut("ResetIdeScaleAction", new KeyboardShortcut(KeyStroke.getKeyStroke("shift control alt 0"), null))
-      }
-
-      ImplicitShortcuts.setShortcuts(ShowImplicitHintsAction.Id, ImplicitShortcuts.EnableShortcuts)
-    }
-
-    override protected def doCleanup(): Unit = {}
-  }
 }
