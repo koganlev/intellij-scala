638d8e71cfc0e13ef2b92128b25646eeff0bad1e
Tobias Kahlert <tobias.kahlert@jetbrains.com>
2025-06-02 14:06:19 +0200

    [irrefutability] fixed stackoverflow #SCL-23913

--- scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/TypedPatternLikeImpl.scala
+++ scala/scala-impl/src/org/jetbrains/plugins/scala/lang/psi/impl/base/patterns/TypedPatternLikeImpl.scala
@@ -7,7 +7,8 @@ import org.jetbrains.plugins.scala.lang.psi.types.result.Typeable
 trait TypedPatternLikeImpl extends ScPattern { this: Typeable =>
 
   final override def isIrrefutableForImpl(scrutineeType: ScType, deep: Boolean): Boolean =
-    `type`().forall { ty =>
-      scrutineeType.conforms(ty) && (!deep || subpatterns.forall(_.isIrrefutableFor(scrutineeType, deep)))
-    }
+    (scrutineeType.isAny ||
+      `type`().forall(scrutineeType.conforms(_))
+      ) &&
+      (!deep || subpatterns.forall(_.isIrrefutableFor(scrutineeType, deep)))
 }
